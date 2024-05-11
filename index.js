4b7cc89ff9e6a6268284ed7ac5a3695dcdf38e3c
aaroon11 <164346730+aaroon11@users.noreply.github.com>
2024-05-11 18:22:13 +0200

    Nuevo cambio
    18:22

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let count = 0
 function increment(){
     count = count + 1
     countEl.textContent = count
+    
 }
 
 function restar(){
@@ -15,7 +16,7 @@ function restar(){
         countEl.textContent = count
     }
     else{
-        alert('No puedes restar personas que no hay')
+        alert('No puede haber personas en negativo')
     }
 }
 
@@ -27,6 +28,47 @@ function save (){
 }
 
 
+let saveEl1 = document.getElementById("save-el1")
+let countEl1 = document.getElementById("count-el1")
+console.log(countEl1)
+
+let count1 = 0
+
+function increment1(){
+    count1 = count1 + 1
+    countEl1.textContent = count1
+}
+
+function restar1(){
+    if (count1 > 0) {
+        count1 = count1 - 1
+        countEl1.textContent = count1
+    }
+    else{
+        alert('No puede haber personas en negativo')
+    }
+}
+
+function save1(){
+    let countStr = count1 + " - "
+    saveEl1.textContent += countStr
+    countEl1.textContent = 0
+    count1 = 0
+  
+}
+
+
+function calcularTotal() {
+    let total = count + count1
+    document.getElementById("total-el").textContent = total
+}
+
+// let countEl2 = document.getElementById("count-el2")
+
+// function total(){
+// total1= count + count1
+// let countStr = total1
+// }
 
 
 
